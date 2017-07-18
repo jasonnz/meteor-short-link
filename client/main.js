b41d59342d1f0f037fd29a7cf41f155c4c81d9b9
Jason Marshall <jasonnz@gmail.com>
2017-07-18 21:18:45 +1200

    Added react-router new routes to project

--- client/main.js
+++ client/main.js
@@ -1,9 +1,26 @@
+// Named imports
 import { Meteor } from 'meteor/meteor';
+import { Router, Route, browserHistory } from 'react-router';
+
+// Imports
+import Login from './../imports/ui/Login';
 import Signup from './../imports/ui/Signup';
 import Link from './../imports/ui/Link';
+import NotFound from './../imports/ui/NotFound';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const routes = (
+  <Router history={browserHistory}>
+    <route path="/" component={Login}/>
+    <route path="/signup" component={Signup}/>
+    <route path="/links" component={Link}/>
+    <route path="*" component={NotFound}/>
+  </Router>
+);
+
+
+
 Meteor.startup(() => {
-  ReactDOM.render(<Link/>, document.getElementById('app'));
+  ReactDOM.render(routes, document.getElementById('app'));
 });
