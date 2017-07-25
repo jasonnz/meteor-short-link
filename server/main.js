49600c4ac8d5f50e52ecad3bcf3010a092a1c2dc
Jason Marshall <jasonnz@gmail.com>
2017-07-26 11:07:56 +1200

    Implemented Simpl-Schema for validation

--- server/main.js
+++ server/main.js
@@ -1,5 +1,31 @@
 import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+  const employeeSchema = new SimpleSchema({
+    name: {
+      type: String,
+      min: 1,
+      max: 200,
+      optional: false
+    },
+    hourlyWage: {
+      type: Number,
+      min: 0
+    },
+    email: {
+      type: String,
+      regEx: SimpleSchema.RegEx.Email
+    }
+
+  });
+
+  employeeSchema.validate({
+    name: 'Jason',
+    hourlyWage: 60,
+    email: 'test@test.com'
+  });
+
 });
