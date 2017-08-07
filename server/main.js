49c8ff04eb8c16b95882d565128e5fa4b24e95cf
Jason Marshall <jasonnz@gmail.com>
2017-08-07 20:55:01 +1200

    Plumbing connect middleware

--- server/main.js
+++ server/main.js
@@ -1,8 +1,17 @@
+import { WebApp } from 'meteor/webapp'
 import { Meteor } from 'meteor/meteor';
 import './../imports/api/users';
 import {Links} from './../imports/api/links';
 import { SimpleSchemaDV } from '../imports/startup/simple-schema-configuration';
 
 Meteor.startup(() => {
-
+    WebApp.connectHandlers.use((req, res, next) => {
+        // console.log('This is from my custom middleware');
+        // // console.log(req.url, ' ' , req.method + ' ', req.headers, ' ----Query---- ', req.query);
+        // res.statusCode = 508;
+        // res.setHeader('my-custom-header', 'Jason was here');
+        // // res.write('<h1>This is my middleware at work</h1>');
+        // res.end();
+        next();
+    });
 });
