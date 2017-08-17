8b647d8a61e9ee85019c8b1b4191258c5cd87372
Jason Marshall <jasonnz@gmail.com>
2017-08-17 21:32:55 +1200

    Added controlled check box filed for LinksList Filters

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -4,16 +4,37 @@ import { Links } from '../api/links';
 
 
 export default class AddLink extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            url: ''
+        }
+    }
+
 
     onSubmit(e) {
-        const url = this.refs.url.value.trim();
+        // const url = this.refs.url.value.trim();
         e.preventDefault();
+        // destructoring
+        const { url } = this.state;
 
         if (url) {
             // Links.insert({ url, userId: Meteor.userId() }); INSECURE
-            Meteor.call('links.insert', url);
-            this.refs.url.value = '';
+            Meteor.call('links.insert', url, (err, res)=> {
+                if (!err) {
+                    this.setState({
+                        url: ''
+                    });
+                }
+            });
+            
         }
+    }
+
+    onChange(e) {
+        this.setState({
+            url: e.target.value.trim()
+        });
     }
 
     render() { 
@@ -21,7 +42,13 @@ export default class AddLink extends React.Component {
             <div>
                 <p>Add Link</p>
                 <form onSubmit={this.onSubmit.bind(this)}>
-                <input type="text" ref="url" placeholder="URL"></input>
+                <input 
+                    type="text" 
+                    ref="url" 
+                    placeholder="URL" 
+                    value={this.state.url}
+                    onChange={this.onChange.bind(this)}
+                />
                 <button>Add Link</button>
                 </form>
             </div>
--- imports/ui/LinksListFilters.js
+++ imports/ui/LinksListFilters.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import { Session } from 'meteor/session';
-// use a stateless functional component
+import { Tracker } from 'meteor/tracker';
 
-export default () => {
-    return (
-        <div>
-            <label>
-                <input type="checkbox" onChange={(e)=> {
-                    // console.log(e.target.checked);
-                    Session.set('showVisible', !e.target.checked);
-                }}/>
-                Show hidden links
-            </label>
-        </div>
-    )
+
+export default class LinksListFilters extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showVisible: false
+        } 
+    }
+
+    componentDidMount() {
+        console.log('Component did mount LinksListFilters');
+        this.linksTracker = Tracker.autorun(() => {
+            this.setState({ showVisible: Session.get('showVisible') });
+            console.log('showVisible: ', this.state.showVisible);
+        });
+    }
+
+    componentWillUnmount() {
+        console.log('Component will unmount LinksListFilters');
+        this.linksTracker.stop();
+    }
+    
+    render() {
+        return (
+            <div>
+                <label>
+                    <input type="checkbox" checked={!this.state.showVisible} onChange={(e)=> {
+                        // console.log(e.target.checked);
+                        Session.set('showVisible', !e.target.checked);
+                    }}/>
+                    Show hidden links
+                </label>
+            </div>
+        )
+    }
 }
